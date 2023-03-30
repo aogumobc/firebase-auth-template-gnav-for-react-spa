ad760475e3b8f295186148b3468e4da5d7e24fc4
aogumobc <aogumobc@gmail.com>
2023-03-30 20:04:46 +0900

    update: add; FontAwesomeIcons

--- src/components/Navigations/GlobalNav/presenter.tsx
+++ src/components/Navigations/GlobalNav/presenter.tsx
@@ -1,4 +1,10 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faHouse,
+    faArrowRightToBracket,
+    faArrowRightFromBracket,
+} from '@fortawesome/free-solid-svg-icons'
 
 export const GlobalNavPresenter: React.FC = () => {
 
@@ -6,14 +12,17 @@ export const GlobalNavPresenter: React.FC = () => {
         <nav className="global-nav">
 
             <Link to={"/"}>
+                <FontAwesomeIcon icon={faHouse} />
                 Home
             </Link>
 
             <Link to={"/login"}>
+                <FontAwesomeIcon icon={ faArrowRightToBracket } />
                 Login
             </Link>
 
             <Link to={"/logout"}>
+                <FontAwesomeIcon icon={ faArrowRightFromBracket } />
                 Logout
             </Link>
 
