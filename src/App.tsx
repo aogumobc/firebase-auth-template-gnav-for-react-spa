7e3551e3317fac52386faa92449cbe70ea6bf810
aogumobc <aogumobc@gmail.com>
2023-03-30 19:34:06 +0900

    add: login/ logout to routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Home } from './components/Pages/Home/container';
+import { Login } from './components/Pages/Login/container';
+import { Logout } from './components/Pages/Logout/container';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="login" element={<Login />} />
+                <Route path="logout" element={<Logout />} />
                 <Route path="*" element={<div>404 Not Found</div>} />
             </Routes>
         </Router>
