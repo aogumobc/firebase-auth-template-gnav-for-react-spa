7333aca6e052fffb7fe85f3fdc9faf8bb0dfb3d6
aogumobc <aogumobc@gmail.com>
2023-03-31 21:21:55 +0900

    update: add the provider for twitter

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, TwitterAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -7,14 +7,17 @@ const firebaseConfig = {
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app      = initializeApp(firebaseConfig);
 const auth     = getAuth(app);
-const provider = new GoogleAuthProvider();
+const provider_google = new GoogleAuthProvider();
+const provider_twitter = new TwitterAuthProvider();
 
 export {
     auth,
-    provider,
+    provider_google,
+    provider_twitter
 }
